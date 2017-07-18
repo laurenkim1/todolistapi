68e7b386f440b4a605b579b66883f8adcae74284
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-17 22:23:31 -0700

    hackermoon

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var app = express();
 var db = require('./db');
 
 var RequestController = require('./request/RequestController');
-app.use('/requests', RequestController);
+app.use('/request', RequestController);
 
 module.exports = app;
