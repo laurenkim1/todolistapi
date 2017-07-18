103e17708f015e99f4cbb956fcc2c94e9ce86426
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-17 22:26:21 -0700

    hackermoon

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
 // server.js
 var app = require('./app');
-
 var port = process.env.PORT || 3000;
-
-Task = require('./api/models/todoListModel');
-bodyParser = require('body-parser');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-var routes = require('./api/routes/todoListRoutes');
-routes(app);
-
 
 var server = app.listen(port, function() {
   console.log('todo list RESTful API server started on: ' + port);
