d555bd1e8845b66f1ee937a7bafb724f6bf6ce74
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-17 21:41:21 -0700

    uuuhh

---
+++ app.js
@@ -0,0 +1,7 @@
+// app.js
+
+var express = require('express');
+var app = express();
+var db = require('./db'); //ADD THIS LINE
+
+module.exports = app;
---
+++ db.js
@@ -0,0 +1,3 @@
+// db.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://mongodb://<laurenkim>:<jihye197>@ds163232.mlab.com:63232/requests');
