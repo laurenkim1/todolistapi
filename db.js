2d895313c554b1dd9e67546e4db0f2fa54a87c64
laurenkim1 <laurenkim@college.harvard.edu>
2017-07-17 22:28:37 -0700

    hackermoon

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 // db.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://mongodb://<laurenkim>:<jihye197>@ds163232.mlab.com:63232/requests');
+mongoose.connect('mongodb://mongodb://laurenkim:jihye197@ds163232.mlab.com:63232/requests');
